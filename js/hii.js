5d3398d238c4c141a928f27a629103866a1a9583
fahdhadhoud <66736069+fahdhadhoud@users.noreply.github.com>
2020-07-06 22:44:54 +0300

    ok

---
+++ js/hii.js
@@ -0,0 +1,43 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  let hok3=document.querySelector('gmg');
+  let hok4=document.querySelector('text');
+  let hok5=document.querySelector('king1');
+
+//   window.onload= function(){
+//     hok3.focus()
+
+//   }
+// function bh(){
+
+
+//     if(hok3.Value){
+// console.log("حاول مرة اخرى")
+//     }
+//     else{
+// let mais=document.childElementCount("span")
+// let tfg= document.childElementCount(hok3)
+
+
+
+
+//     }
+//   }
+
+let tyi=[];
+
+function uu(){
+
+ let ook=   document.getElementsByClassName("ipk").Value
+ tyi.push( ook);
+  let hjl= document.getElementById("ui").innerHTML += "<li>"  +ook+ "</li>";
+
+}
